b1b7e79b3bb9e869b322f9cf7685ab5359d5f76a
dennisSaralino <djsaralino@gmail.com>
2024-03-12 19:08:41 -0400

    topic-07

--- topic-07-quotes/quotes.py
+++ topic-07-quotes/quotes.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, request # redirect
+from flask import Flask, render_template, redirect # redirect
 from mongita import MongitaClientDisk
+from bson import ObjectId
+
+app = Flask(__name__)
 
 # open a mongita client connection
 client = MongitaClientDisk()
 
 # open a quote database
 quotes_db = client.quotes_db
-
-app = Flask(__name__)
-
-# pulls info from URL parameters from form
 
 # GET method
 @app.route("/", methods=["GET"])
@@ -19,6 +18,7 @@ def get_quotes():
     data = list(quotes_collection.find({}))
     for item in data:
         item["_id"] = str(item["_id"])
+        item["object"] = ObjectId(item["_id"])
     print(data)
     return render_template("quotes.html", data=data)
 
@@ -29,4 +29,5 @@ def get_delete(id=None):
         # open the quotes collection
         quotes_collection = quotes_db.quotes_collection
         #delete the item
-        quotes_collection.delete_one({'_id'==id})
+        quotes_collection.delete_one("_id"==ObjectId(id))
+        return redirect("/quotes")
